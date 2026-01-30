fdd5d655f0ab1c99b58bb7d44c3adbdb6d25d625
Saaammmyyy <107764282+Saaammmyyy@users.noreply.github.com>
2026-01-30 21:20:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: ""C:\Users\Kirthi\Downloads\Edd_Sheeran_-_Perfect_(mp3.pm).mp3"", // Music streaming URL
+        musicUrl: ""https:/res.cloudinary.com/dfhwpwfit/video/upload/Edd_Sheeran_-_Perfect__mp3.pm_yy6sxk.mp3"", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
